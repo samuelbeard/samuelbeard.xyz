b2bb982c790149e67568f6829f9b0b31393e5540
samuelbeard <samjb2@gmail.com>
2022-01-25 14:22:23 +0000

    stuff

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head"
+import Image from "next/image"
 
 export default function Home() {
-  return (
-    <div className="h-screen w-screen bg-slate-500 flex justify-center items-center">
-      <h1>Samuel Beard</h1>
-    </div>
-  )
+    return (
+        <div className="background h-screen w-screen bg-slate-600 flex justify-center items-center flex-col">
+            <h1 className="text-slate-200 text-6xl font-bold uppercase">
+                Samuel Beard
+            </h1>
+            <h2 className="text-slate-400 text-2xl font-semibold">
+                I'll get around to building something here soon...
+            </h2>
+        </div>
+    )
 }
